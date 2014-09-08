abb04165292c2bc0af55bdb5fb1e45c4c8e2c9ca
Matt Waechter & Andrew M Westling <student@epicodus.com>
2014-09-08 14:58:32 -0700

    Add callback to Item model to capitalize name on input

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ActiveRecord::Base
   has_many :photos
+  before_save :capitalize_name
   validates :name, :presence => true
+
+  private
+
+    def capitalize_name
+      self.name = self.name.capitalize
+    end
 end
