bf60b6828a4e725755f3fe064a030d17e5ad856b
Matt Waechter & Andrew M Westling <student@epicodus.com>
2014-09-09 16:18:45 -0700

    add seeds.rb for seeding test to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Item.create({name: 'Copenhagen'})
+Item.create({name: 'Stockholm'})
+Item.create({name: 'Vancouver'})
+Item.create({name: 'Shanghai'})
+User.create({email: 'hi@aol.com', password: 'go'})
+Photo.create({:item_image => File.open("#{Rails.root}/public/system/cities/cph.jpg"), :item_id => 1})
+Photo.create({:item_image => File.open("#{Rails.root}/public/system/cities/sthlm.jpg"), :item_id => 2})
+Photo.create({:item_image => File.open("#{Rails.root}/public/system/cities/van.jpg"), :item_id => 3})
+Photo.create({:item_image => File.open("#{Rails.root}/public/system/cities/shang.jpg"), :item_id => 4})
+
